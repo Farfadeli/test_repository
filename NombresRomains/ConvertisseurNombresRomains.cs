f0b16386f842d5d84d8b69dfc0253b6c5c2963cd
ndaunac <ndaunac@gmail.com>
2023-10-25 22:56:23 +0200

    is commit with python

--- NombresRomains/ConvertisseurNombresRomains.cs
+++ NombresRomains/ConvertisseurNombresRomains.cs
@@ -4,6 +4,10 @@ public static class ConvertisseurNombresRomains
 {
     public static string Convertir(uint chiffre)
     {
+        if(chiffre == 5)
+        {
+            return "V";
+        }
         return chiffre == 1 ? "I" :  chiffre == 2 ? "II" : "III";
     }
 }
